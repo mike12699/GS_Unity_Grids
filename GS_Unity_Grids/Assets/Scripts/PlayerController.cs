de003b32ea735fc588b646f1127909044fe1da00
mike12699 <335565@colum.edu>
2021-09-23 21:34:14 -0500

    Adds the grid and player movement
    Player grid movement and grid layout included.

---
+++ GS_Unity_Grids/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    Rigidbody2D rb;
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    private float timeToMove = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.left));
+        }
+
+        if (Input.GetKey(KeyCode.W) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.up));
+        }
+
+        if (Input.GetKey(KeyCode.D) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.right));
+        }
+
+        if (Input.GetKey(KeyCode.S) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.down));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("hit detected");
+        GameObject p = Instantiate(player) as GameObject;
+        //Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
